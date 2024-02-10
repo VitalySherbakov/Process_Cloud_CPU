0cd0e7a3befea4345aeefa2895ddf5d44f9fba01
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2024-02-10 03:18:38 +0300

    1561

--- Linex_Main.py
+++ Linex_Main.py
@@ -34,7 +34,7 @@ while True:
     if not os.path.exists(dir_dircts):
         os.system(f'mkdir "{dir_dircts}"')
     if not os.path.exists(dir_pass):
-        os.system(f"mkdir "{dir_pass}"') 
+        os.system(f'mkdir "{dir_pass}"') 
     Dict_Download = app.ReadJson(app.SettingDicts)
     current_date = datetime.datetime.now()
     current_date_str = current_date.strftime("%d.%m.%Y %H:%M:%S")
