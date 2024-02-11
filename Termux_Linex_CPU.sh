385779521eb50d59e8dd8c22fdcf8069bffd3513
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2024-02-11 20:44:29 +0300

    5616151

--- Termux_Linex_CPU.sh
+++ Termux_Linex_CPU.sh
@@ -7,14 +7,14 @@ dirsource="Process_Cloud_CPU"
 # ----------------------Functions----------------------
 function function_install_cpu(){
 	# Установка CPU
-	apt-get update -y
+	apt update -y
 	git clone https://github.com/aircrack-ng/aircrack-ng
     cd aircrack-ng
     bash "./autogen.sh"
     "./configure"
     make
     sudo make install
-	apt-get update -y
+	apt update -y
     apt install aircrack-ng -y
 	aircrack-ng --help
 	cd ..
@@ -22,24 +22,24 @@ function function_install_cpu(){
 function function_pack(){
     # Установка Пакетов
     echo "Обновление..."
-    apt-get update -y
+    apt update -y
     echo "Загрузка Пакетов 1..."
-    apt-get install sudo -y
-    sudo apt-get install ssh -y
-    sudo apt-get install curl -y
-    sudo apt-get install wget -y
-    sudo apt-get install git -y
-    sudo apt-get install rar -y
-    sudo apt-get install zip -y
-    sudo apt-get install p7zip-full -y
-	sudo apt-get install unrar-free -y
-    apt-get update -y
+    apt install sudo -y
+    sudo apt install ssh -y
+    sudo apt install curl -y
+    sudo apt install wget -y
+    sudo apt install git -y
+    sudo apt install rar -y
+    sudo apt install zip -y
+    sudo apt install p7zip-full -y
+	sudo apt install unrar-free -y
+    apt update -y
     echo "Загрузка Пакетов 2..."
-    apt-get install -y build-essential
-    DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+    apt install -y build-essential
+    DEBIAN_FRONTEND=noninteractive apt install -y tzdata
     apt install -y software-properties-common
     add-apt-repository -y ppa:deadsnakes/ppa
-    apt-get update -y
+    apt update -y
     apt install -y python3.8 python3-pip
     echo "Загрузка Пакетов 3..."
     python3.8 --version
@@ -52,6 +52,40 @@ function function_pack(){
     python3.8 -m pip install tqdm
     python3.8 -m pip install py7zr
     python3.8 -m pip install rarfile
+    echo "-----Конец Установки Пакетов-----"
+}
+function function_pack2(){
+    # Установка Пакетов
+    echo "Обновление..."
+    apt update -y
+    echo "Загрузка Пакетов 1..."
+    apt install sudo -y
+    sudo apt install ssh -y
+    sudo apt install curl -y
+    sudo apt install wget -y
+    sudo apt install git -y
+    sudo apt install rar -y
+    sudo apt install zip -y
+    sudo apt install p7zip-full -y
+	sudo apt install unrar-free -y
+    apt update -y
+    echo "Загрузка Пакетов 2..."
+    apt install -y build-essential
+    apt install -y software-properties-common
+    add-apt-repository -y ppa:deadsnakes/ppa
+    apt update -y
+    apt install -y python3.10 python3-pip
+    echo "Загрузка Пакетов 3..."
+    python3.10 --version
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo python3.10 get-pip.py
+    rm -r get-pip.py
+    pip install --upgrade pip
+    python3.10 -m pip install requests
+    python3.10 -m pip install alive-progress
+    python3.10 -m pip install tqdm
+    python3.10 -m pip install py7zr
+    python3.10 -m pip install rarfile
     echo "-----Конец Установки Пакетов-----"
 }
 function access_ubuntu(){
@@ -71,14 +105,14 @@ function main(){
     read command
     if [ "$command" == "pack" ]; then
         access_ubuntu
-		function_pack
+		function_pack2
 	fi
     if [ "$command" == "cpu" ]; then
         access_ubuntu
 		function_install_cpu
 	fi
     if [ "$command" == "run" ]; then
-        python3.8 "./$dirsource/Linex_Main.py" "$1"
+        python3.10 "./$dirsource/Linex_Main.py" "$1"
 	fi
     if [ "$command" == "exit" ]; then
 		#break
