8cd40cd8cfb7f28d004d0f64ee181694e2b02b53
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2024-02-12 00:53:46 +0300

    56151

--- Termux_Linex_CPU.sh
+++ Termux_Linex_CPU.sh
@@ -44,41 +44,41 @@ function function_python(){
 	python3.8 -m pip install rarfile
     python3.8 -m pip install urllib3==1.26.7
 }
-function function_pack(){
-    # Установка Пакетов
-    echo "Обновление..."
-    apt update -y
-    echo "Загрузка Пакетов 1..."
-    apt install sudo -y
-    sudo apt install ssh -y
-    sudo apt install curl -y
-    sudo apt install wget -y
-    sudo apt install git -y
-    sudo apt install rar -y
-    sudo apt install zip -y
-    sudo apt install p7zip-full -y
-	sudo apt install unrar-free -y
-    apt update -y
-    echo "Загрузка Пакетов 2..."
-    apt install -y build-essential
-    DEBIAN_FRONTEND=noninteractive apt install -y tzdata
-    apt install -y software-properties-common
-    add-apt-repository -y ppa:deadsnakes/ppa
-    apt update -y
-    apt install -y python3.8 python3-pip
-    echo "Загрузка Пакетов 3..."
-    python3.8 --version
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    sudo python3.8 get-pip.py
-    rm -r get-pip.py
-    pip install --upgrade pip
-    python3.8 -m pip install requests
-    python3.8 -m pip install alive-progress
-    python3.8 -m pip install tqdm
-    python3.8 -m pip install py7zr
-    python3.8 -m pip install rarfile
-    echo "-----Конец Установки Пакетов-----"
-}
+# function function_pack(){
+#     # Установка Пакетов
+#     echo "Обновление..."
+#     apt update -y
+#     echo "Загрузка Пакетов 1..."
+#     apt install sudo -y
+#     sudo apt install ssh -y
+#     sudo apt install curl -y
+#     sudo apt install wget -y
+#     sudo apt install git -y
+#     sudo apt install rar -y
+#     sudo apt install zip -y
+#     sudo apt install p7zip-full -y
+# 	sudo apt install unrar-free -y
+#     apt update -y
+#     echo "Загрузка Пакетов 2..."
+#     apt install -y build-essential
+#     DEBIAN_FRONTEND=noninteractive apt install -y tzdata
+#     apt install -y software-properties-common
+#     add-apt-repository -y ppa:deadsnakes/ppa
+#     apt update -y
+#     apt install -y python3.8 python3-pip
+#     echo "Загрузка Пакетов 3..."
+#     python3.8 --version
+#     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#     sudo python3.8 get-pip.py
+#     rm -r get-pip.py
+#     pip install --upgrade pip
+#     python3.8 -m pip install requests
+#     python3.8 -m pip install alive-progress
+#     python3.8 -m pip install tqdm
+#     python3.8 -m pip install py7zr
+#     python3.8 -m pip install rarfile
+#     echo "-----Конец Установки Пакетов-----"
+# }
 function function_pack2(){
     # Установка Пакетов
     echo "Обновление..."
@@ -95,23 +95,7 @@ function function_pack2(){
 	apt install unrar-free -y
     apt update -y
     echo "Загрузка Пакетов 2..."
-    apt install -y build-essential
-    apt install -y software-properties-common
-    add-apt-repository -y ppa:deadsnakes/ppa
-    apt update -y
-    apt install -y python python-pip
-    echo "Загрузка Пакетов 3..."
-    python --version
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
-    chmod -R 777 ./get-pip.py
-    rm -r get-pip.py
-    pip install --upgrade pip
-    python -m pip install requests
-    python -m pip install alive-progress
-    python -m pip install tqdm
-    python -m pip install py7zr
-    python -m pip install rarfile
+    function_python
     echo "-----Конец Установки Пакетов-----"
 }
 function access_ubuntu(){
@@ -131,14 +115,14 @@ function main(){
     read command
     if [ "$command" == "pack" ]; then
         access_ubuntu
-		function_python
+		function_pack2
 	fi
     if [ "$command" == "cpu" ]; then
         access_ubuntu
 		function_install_cpu
 	fi
     if [ "$command" == "run" ]; then
-        python "./$dirsource/Linex_Main.py" "$1"
+        python3.8 "./$dirsource/Linex_Main.py" "$1"
 	fi
     if [ "$command" == "exit" ]; then
 		#break
